61dc8652204f27b97f008a4d51081433917933e5
Diego <diego_cala@soy.sena.edu.co>
2024-09-09 11:21:57 -0500

    correciones

--- ContacctoApp/views.py
+++ ContacctoApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import FormContacto
 from django.core.mail import EmailMessage
 from MiProyecto import settings
@@ -8,17 +8,24 @@ def contacto(request):
     
     form_contacto = FormContacto()
     
-    if request.method =="POST":
-        form_contacto = FormContacto(datos = request.POST)
-        if form_contacto.is_valid:
+    if request.method == "POST":
+        form_contacto = FormContacto(data=request.POST)
+        if form_contacto.is_valid():
             nombre = request.POST.get("nombre")
-            correo = equest.POST.get("correi")
+            correo = request.POST.get("correo")
             mensaje = request.POST.get("mensaje")
             
-            email = EmailMessage("Mensaje desde Mi proyecto",
-                                 "El usuario {} con direccion de correo{} escribio lo siguiente"
-                                 .format(nombre, correo, mensaje), settings.EMAIL_HOST_USER,
-                                 correo, reply_to=[correo])
-            
+            email = EmailMessage(
+                "Mensaje desde Mi proyecto",
+                f"El usuario {nombre} con dirección de correo {correo} escribió lo siguiente:\n\n{mensaje}",
+                settings.EMAIL_HOST_USER, 
+                ["rogersprof@gmail.com"], 
+                reply_to=[correo]
+            )
+            try:
+                email.send()
+                return redirect("/contacto/?ok")
+            except:
+                return redirect("/contacto/?error")
             
     return render(request, "ContactoApp/contacto.html", {'formulario': form_contacto})
